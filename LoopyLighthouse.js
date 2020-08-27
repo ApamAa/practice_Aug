17228f0f099c89571758cfa217f170950443a102
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-08-27 16:15:38 -0700

    loopylighthouse

---
+++ LoopyLighthouse.js
@@ -0,0 +1,12 @@
+for(let n = 100; n <= 200; n++) {
+  if(n % 3 === 0 && n % 4 === 0) {
+    console.log('loopylighthouse')
+  } else if(n % 3 === 0) {
+      console.log('loopy')
+    
+  } else if(n % 4 === 0) {
+    console.log('lighthouse')
+  } else{
+  console.log(n)
+  }
+};
