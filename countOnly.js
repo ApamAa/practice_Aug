722aa949d77bbf2e6bf96459c24d143f9bb9287d
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-09-02 09:09:32 -0700

    functions made and tests are passed

---
+++ countOnly.js
@@ -0,0 +1,36 @@
+const asserEqual = require('./assertEqual');
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+asserEqual(result1["Jason"], 1);
+asserEqual(result1["Karima"], undefined);
+asserEqual(result1["Fang"], 2);
+asserEqual(result1["Agouhanna"], undefined);
