64d2fa31f116e3ac0c9089b48d57341b5340c7ef
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-09-07 14:11:33 -0700

    modify the code to return the index

---
+++ findWaldo.js
@@ -0,0 +1,15 @@
+const findWaldo = function(names, found) {
+   names.forEach((name, i)=> {
+     
+    if (name === "Waldo") {
+      found(i);   // execute callback
+    }
+  });
+};
+
+const actionWhenFound = function(index) {
+  console.log("Found him at index : " + index);
+  
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
