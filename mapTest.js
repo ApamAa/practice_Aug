bfd8e6a6a8940918fcbf82fd9fdb0acaf84663c8
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-09-08 14:41:42 -0700

    arrow function and map

---
+++ mapTest.js
@@ -0,0 +1,6 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+
+const mapTest = lighthouses.map(x => x.length);
+
+console.log(mapTest);
