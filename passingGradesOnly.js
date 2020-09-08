9004fd7dfead42920da4b85ec031c1a56b8ddf1a
Apama Jamshidi <apama.jamshidi@gmail.com>
2020-09-08 14:35:14 -0700

    inline function with using array.prototuype

---
+++ passingGradesOnly.js
@@ -0,0 +1,14 @@
+
+
+
+
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+
+const paasingGradesOnly = grades.filter(num => {
+  return num >=70
+});
+
+
+
+
+console.log(paasingGradesOnly);
